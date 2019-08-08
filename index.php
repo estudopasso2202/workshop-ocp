faa3c49fc3f468241036360515bad04a72e93ff3
estudopasso2202 <estudopasso2202@gmail.com>
2019-08-08 19:50:14 +0000

    v3.0

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-echo "<h1>Openshift Workshop v2.0</h1> ";
-echo $_SERVER['SERVER_ADDR'];
-echo "<br><hr>";
+echo "<h1>Openshift Workshop v3.0</h1> ";
+echo $_SERVER['SERVER_ADDR']
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
 $conn = new mysqli("mysql", "redhat", "redhat", "workshop");
 if ($conn->connect_error) {
